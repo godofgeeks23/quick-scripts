beaaa56a63517e09ef92c19a11d65483049c33df
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-09-12 23:26:27 +0530

    ZSH, Plugins and Powerlevel10k setup script

---
+++ zsh_powerlevelsetup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install wget zsh git fonts-font-awesome
+# create an empty .zshrc file
+# touch ~/.zshrc
+zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm ~/.zshrc
+cp sample.zshrc ~/.zshrc
+zsh
