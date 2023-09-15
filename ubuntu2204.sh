1dc3eea6c1e6a19180daed080afbd49c2d74bf38
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-09-15 22:20:50 +0530

    Update ubuntu2204.sh

--- ubuntu2204.sh
+++ ubuntu2204.sh
@@ -1,16 +1,8 @@
 sudo apt update
-
 sudo apt remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku 
-
 sudo apt autoremove
-
 sudo apt install torsocks curl synaptic neofetch gcc g++ htop vlc software-properties-common apt-transport-https 
-
 wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
-
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
-
 sudo apt update
-
 sudo apt install code
-
