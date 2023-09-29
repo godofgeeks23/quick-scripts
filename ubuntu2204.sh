0b25f9eb3dbe1b65d6b8f1355c8f65a0246628ae
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-09-29 23:19:42 +0530

    Update ubuntu2204.sh

--- ubuntu2204.sh
+++ ubuntu2204.sh
@@ -1,9 +1,9 @@
 # a general script to setup a fresh install of ubuntu
 
 sudo apt update
-sudo apt remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku 
+sudo apt remove aisleriot gnome-mahjongg gnome-mines gnome-sudoku remmina transmission thundebird rhythmbox
 sudo apt autoremove
-sudo apt install torsocks curl synaptic neofetch gcc g++ htop vlc software-properties-common apt-transport-https 
+sudo apt install curl synaptic neofetch gcc g++ htop vlc software-properties-common apt-transport-https 
 wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
 echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
 sudo apt update
