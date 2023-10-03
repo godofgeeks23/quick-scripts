da416596f6e8146e5bed30de1054e3bbc16c0d4e
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-10-03 23:25:03 +0530

    Update ubuntu2204.sh

--- ubuntu2204.sh
+++ ubuntu2204.sh
@@ -34,9 +34,8 @@ sudo docker run hello-world
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install node
+nvm install node # install latest node and npm
 
 # install conda - for python
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-
