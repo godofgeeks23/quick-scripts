2d6580c62dd6cd5c28434a7e163a7989cf7531c9
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-03-11 15:01:17 +0530

    Update docker_jenkins_setup.sh

--- docker_jenkins_setup.sh
+++ docker_jenkins_setup.sh
@@ -13,17 +13,17 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
 sudo apt update
-sudo apt install openjdk-17-jre
+sudo apt install -y openjdk-17-jre
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install -y jenkins
 sudo usermod -a -G docker jenkins
 
 # end of script
